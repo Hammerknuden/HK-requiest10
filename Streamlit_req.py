937ecca25d87f42caf6e07ffa0a26b2329b4f2c1
Finn <finnjorg@>
2025-02-19 15:50:14 +0100

    update still positional fault  calc ok19 feb 1550

--- Streamlit_req.py
+++ Streamlit_req.py
@@ -218,13 +218,13 @@ if sprog == "dansk" and booking_submitted:
     st.markdown('forespørgelse er afsendt dette vindue kan lukkes')
 
 elif sprog == "english" and booking_submitted:
-    send_danish_confirmation_email(to_addr, confirmation_password, navn, num_rooms, num_personer, checkin_date, checkout_date,
-                                   text_bf, text_bed, text_free, pris_tot)
+    send_danish_confirmation_email(to_addr, confirmation_password, navn, num_rooms, num_personer, checkin_date,
+                                   checkout_date, text_sing, text_bf, text_bed, text_free, pris_tot)
     st.markdown('Your request has been send, this window can be closed')
 
 elif sprog == "Deutsch" and booking_submitted:
-    send_german_confirmation_email(to_addr, confirmation_password, navn, num_rooms, num_personer, checkin_date, checkout_date,
-                                   text_bf, text_bed, text_free, pris_tot)
+    send_german_confirmation_email(to_addr, confirmation_password, navn, num_rooms, num_personer, checkin_date,
+                                   checkout_date, text_sing, text_bf, text_bed, text_free, pris_tot)
 
 else:
     st.markdown('forespørgelsen ikke sendt, sendt mail direkte til mail@hammerknuden.dk')
