8ba6f22632651450e52063c3cb775c64d9e5145d
Finn <finnjorg@>
2025-02-28 15:01:55 +0100

    update smalll edit  28 feb  1501

--- confirmation_email.py
+++ confirmation_email.py
@@ -159,9 +159,9 @@ def send_english_confirmation_email(to_addr, confirmation_password, navn, num_ro
                                     checkin_date, checkout_date, text_sing, text_bf, text_bed, text_free, pris_tot,
                                     text_price, email_address, telefon):
     logo_cid = make_msgid()
-    html_content = danish_email_html_template(logo_cid[1:-1], navn, num_rooms, num_personer, checkin_date,
-                                              checkout_date, text_sing, text_bf, text_bed, text_free, pris_tot,
-                                              text_price, email_address, telefon)
+    html_content = english_email_html_template(logo_cid[1:-1], navn, num_rooms, num_personer, checkin_date,
+                                               checkout_date, text_sing, text_bf, text_bed, text_free, pris_tot,
+                                               text_price, email_address, telefon)
     # construct email
     email = EmailMessage()
 
